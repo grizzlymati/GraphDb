c94849b0a5d0e512006d268ab2f1d5f911faae58
Mateusz Lis <lismateusz1995@gmail.com>
2019-12-22 18:44:19 +0100

    init

---
+++ Neo4JSample.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Neo4JSample.ConsoleApp.Services;
+using System.Threading.Tasks;
+using Neo4JSample.Settings;
+
+namespace Neo4JSample.ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new MovieDataService();
+
+            RunAsync(service).GetAwaiter().GetResult();
+        }
+
+        public static async Task RunAsync(IMovieDataService service)
+        {
+            var settings = ConnectionSettings.CreateBasicAuth("bolt://hobby-pfoigaoofoojgbkejnnpffdl.dbs.graphenedb.com:24787", "mateo", "b.W43aW5nzmslv.h1dNlQp5mVRQcJVM");
+
+            using (var client = new Neo4JClient(settings))
+            {
+                service.PrepareDataForDb();
+
+                // Create Indices for faster Lookups:
+                await client.CreateIndices();
+
+                // Create Base Data:
+                await client.CreateMovies(service.Movies);
+                await client.CreatePersons(service.Persons);
+                await client.CreateGenres(service.Genres);
+
+                // Create Relationships:
+                await client.CreateRelationships(service.Metadatas);
+            }
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Neo4JSample;
+using Neo4JSample.Model;
+using Neo4JSample.Settings;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ConnectionSettings settings = ConnectionSettings.CreateBasicAuth("bolt://hobby-pfoigaoofoojgbkejnnpffdl.dbs.graphenedb.com:24787", "mateo", "b.W43aW5nzmslv.h1dNlQp5mVRQcJVM");
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult GetMovies()
+        {
+            using (var client = new Neo4JClient(settings))
+            {
+                IList<Movie> result = client.GetSpecificMovies(null);
+                return Json(result);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetPersons(string movieType, string movieTitle, string personsRelation)
+        {
+            using (var client = new Neo4JClient(settings))
+            {
+                IList<Person> result = client.GetSpecificPersons(movieType, movieTitle, personsRelation);
+                return Json(result);
+            }
+        }
+    }
+}
